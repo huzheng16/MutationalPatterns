02798e1fffd6835e7f85db88eaa0aead2fb3f51a
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-03-31 13:06:43 +0200

    add fit to signatures function

---
+++ R/fit_to_signatures.R
@@ -0,0 +1,30 @@
+#' Find optimal nonnegative linear combination of mutation signatures to reconstruct the mutation matrix
+#' 
+#' @description Find linear combination of mutation signatures that most closely reconstructs the mutation matrix by solving nonnegative least-squares constraints problem
+#' @param mut_matrix 96 mutation count matrix (dimensions: 96 mutations X n samples)
+#' @param signatures Signature matrix (dimensions: 96 mutations X n signatures)
+#' @return Named list with signature contributions and reconstructed mutation matrix
+#' @export
+
+fit_to_signatures = function(mut_matrix, signatures)
+{
+  # make sure dimensions of input matrix are correct
+  if(!(dim(mut_matrix)[1] == 96))stop("Mutation count matrix input should have dimensions 96 X n samples")
+  if(!(dim(mut_matrix)[1] == 96))stop("Signatures input should have dimensions 96 X n signatures")
+  n_samples = dim(mut_matrix)[2]
+  n_signatures = dim(signatures)[2]
+  lsq_contribution = matrix(NA, nrow=n_signatures, ncol=n_samples)
+  lsq_reconstructed = matrix(NA, nrow=96, ncol=n_samples)
+  # for each sample
+  for(i in 1:ncol(mut_matrix))
+  {
+    y = mut_matrix[,i]
+    lsq = lsqnonneg(signatures, y)
+    lsq_contribution[,i] = lsq$x
+    lsq_reconstructed[,i] = signatures %*% as.matrix(lsq$x) 
+  }
+  colnames(lsq_contribution) = colnames(mut_matrix)
+  res = list(lsq_contribution, lsq_reconstructed)
+  names(res) = c("contribution", "reconstructed")
+  return(res)
+}
