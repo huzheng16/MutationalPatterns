6a91dfda9f77fd77d59b1bb5f7fc9a4ff7f086f6
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-04-01 12:44:48 +0200

    one plot 96 profile function for both signatures and 96 spectrum

--- R/plot_96_profile.R
+++ R/plot_96_profile.R
@@ -1,27 +1,20 @@
-#' Plot 96 trinucleotide signatures 
-#'  
-#' @param vcf_files Character vector of vcf_files
-#' @param rank Number of signatures one would like to extract
-#' @param outdir Specify output directory
+#' Plot 96 trinucleotide changes spectrum
+#'    
+#' @param mut_spectrum_96 96 trinucleotide changes count matrix
+#' @return plot 96 trinucleotide changes spectrum plot
 #' @export
 
-plot_signatures_96 = function(signatures, colors = c("#DBD7C8", "#B2D39C", "#71C1BA", "#2DAFCE", "#2476B2", "#737E93"))
-{
+
+plot_96_profile = function(mut_spectrum_96, colors = c("#DBD7C8", "#B2D39C", "#71C1BA", "#2DAFCE", "#2476B2", "#737E93")){
   # check color vector length
   if(length(colors) != 6){stop("Color vector length not 6")}
-  # Variables for signature plotting
+  signatures = t(mut_spectrum_96)  
   substitutions = c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')
   index = c(rep(1,1,16), rep(2,1,16), rep(3,1,16), rep(4,1,16), rep(5,1,16), rep(6,1,16))
   # Relative contribution
   norm_signatures = apply(signatures, 2, function(x) x / sum(x) )
-  # Signature colnames
-  names = c()
-  for(i in 1:ncol(signatures)){names = c(names,(paste("Signature", as.character(i))))}
-  colnames(norm_signatures) = names
   # Context
   context = rownames(norm_signatures)
-  # remove part after dot
-  context = sapply(context, function(x) strsplit(x, "\\.")[[1]][1])
   # Replace mutated base with dot
   substring(context,2,2) = "."
   # Construct dataframe
@@ -29,13 +22,14 @@ plot_signatures_96 = function(signatures, colors = c("#DBD7C8", "#B2D39C", "#71C
   rownames(norm_signatures) = NULL
   df2 = cbind(df, as.data.frame(norm_signatures))
   df3 = melt(df2, id.vars = c("substitution", "context"))
-  plot = ggplot(data=df3, aes(x=context, y=value, fill=substitution, width=0.6)) +  
-          geom_bar(stat="identity", colour="black", size=.1) + 
+  
+  print(ggplot(data=df3, aes(x=context, y=value, fill=substitution, width=0.6)) +  
+          geom_bar(stat="identity", colour="black", size=.2) + 
           scale_fill_manual(values=colors) + 
           facet_grid(variable ~ substitution) + 
           ylab("Relative contribution") + 
-          coord_cartesian(ylim=c(0,0.25)) +
-          scale_y_continuous(breaks=c(0, 0.1, 0.2)) +
+          coord_cartesian(ylim=c(0,0.15)) +
+          scale_y_continuous(breaks=c(0, 0.1)) +
           # no legend
           guides(fill=FALSE) + 
           # white background
@@ -46,7 +40,6 @@ plot_signatures_96 = function(signatures, colors = c("#DBD7C8", "#B2D39C", "#71C
                 axis.title.x=element_text(size=12),
                 axis.text.x=element_text(size=5,angle=90,vjust=0.4),
                 strip.text.x=element_text(size=14),
-                strip.text.y=element_text(size=14),
-                panel.grid.major.x = element_blank())
-  return(plot)
+                strip.text.y=element_text(size=14, angle=360),
+                panel.grid.major.x = element_blank()))
 }
