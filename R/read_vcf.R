1ba13db22e8a795884df91ba189d49997fab86f1
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-07-04 13:57:14 +0200

    Only store rowRanges of vcf in object to use less memory and speed up processes. Also remove positions with indels and multiple alternative alleles.

--- R/read_vcf.R
+++ R/read_vcf.R
@@ -11,26 +11,20 @@
 
 read_vcf = function(vcf_files, sample_names, genome = "-")
 {
+  # check sample names
   if(!(length(vcf_files) == length(sample_names))){stop("Provide the same number of sample names as vcf files")}
+  dna_bases = c("A","C","G","T")
+  # make list with vcf objects
   vcf_list = list()
   for(i in 1:length(vcf_files))
   {
     vcf = readVcf(vcf_files[i], genome)
-    # remove positions with multiple alternative alleles
-    mult_alt_all = which(lapply(alt(vcf), function(x) length(x) > 1) == TRUE)
-    if(length(mult_alt_all > 0)){
-      warning(paste(sample_names[i],"contains", length(mult_alt_all), "position(s) with multiple alternative alleles. These positions were excluded."))
-      vcf = vcf[-mult_alt_all]
+    vcf = rowRanges(vcf)
+    rem = which(all(!( vcf$ALT %in% dna_bases & vcf$REF %in% dna_bases & (lengths(vcf$ALT) == 1) )))
+    if(length(rem) > 0) {
+      vcf = vcf[-rem]
+      warning(length(rem), " position(s) with indels and multiple alternative alleles are removed.")
     }
-    # remove indel positions
-    insertions = which(unlist(lapply(alt(vcf), function(x) width(x))) > 1)
-    deletions = which(width(ref(vcf)) > 1)
-    indels = c(insertions, deletions)
-    if(length(indels > 0)){
-      warning(paste(sample_names[i],"contains", length(indels), "indel position(s). These positions were excluded."))
-      vcf = vcf[-indels]
-    }
-    
     # add "chr" to chromosomes if not there already
     # if(length(grep("chr", seqlevels(vcf))) == 0){seqlevels(vcf) = paste('chr', seqlevels(vcf), sep = '')}
     vcf = list(vcf)
@@ -39,4 +33,6 @@ read_vcf = function(vcf_files, sample_names, genome = "-")
   }
   return(vcf_list)
 }
+
+
 
