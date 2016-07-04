9d2bb097482811e5c273ab2659a6fdd2545cd976
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-07-04 14:06:33 +0200

    Resize granges object of vcf for context retrieval from ref genome (=faster)

--- R/get_mut_context.R
+++ R/get_mut_context.R
@@ -1,13 +1,14 @@
 #' Retrieve context of base substitutions
 #' 
-#' A function to extract the bases 3' upstream and 5' downstream of the base substitutions
-#' @param vcf A CollapsedVCF object
+#' A function to extract the bases 3' upstream and 5' downstream of the base substitutions from the reference genome
+#' @param vcf A Granges object
 #' @param ref_genome Reference genome
-#' @return Character vector the context of the base substitutions
+#' @return Character vector with the context of the base substitutions
 #' @export
 
 get_mut_context = function(vcf, ref_genome) 
 {
-  vcf_context = as.character(getSeq(get(ref_genome), seqnames(vcf), start(vcf) - 1, end(vcf) + 1))
+  ranges = resize(vcf, 3, fix = "center")
+  vcf_context = getSeq(get(ref_genome), ranges)
   return(vcf_context)
 }
