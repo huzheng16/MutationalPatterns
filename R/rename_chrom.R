bab44eb875d14550f9bdc3e065c9dfef58631148
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-07-08 12:15:20 +0200

    Rename chromosome function

---
+++ R/rename_chrom.R
@@ -0,0 +1,19 @@
+#' Rename chromosome names to standard
+#' 
+#' @param granges Granges object with chromosome names to rename
+#' @param style "UCSC", NCBI" or "Ensembl", default = "UCSC"
+#' @return granges Granges object with renamed chromosomes according to style
+#' @export
+
+rename_chrom = function(granges, style = "UCSC")
+{
+  # rename mitochondrial DNA manually
+  seqlevels(granges)[seqlevels(granges)=="chrMT"] = "chrM"
+  # get chromosome style
+  chrom_style = mapSeqlevels(seqlevels(granges), style)
+  # removing NA cases
+  chrom_style = chrom_style[complete.cases(chrom_style)] 
+  # rename chromosome names (seqlevels)
+  res = renameSeqlevels(granges, chrom_style)
+  return(res)
+}
