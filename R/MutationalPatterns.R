fcd0072f64780d6d4aa2683a4370dcb6e762370c
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-07-12 13:47:27 +0200

    Global package variables in capital

--- R/MutationalPatterns.R
+++ R/MutationalPatterns.R
@@ -1,3 +1,12 @@
-spectrum.colors6 = c("#2EBAED", "#000000", "#DE1C14", "#D4D2D2", "#ADCC54", "#F0D0CE")
-spectrum.colors7 = c("#2EBAED", "#000000", "#DE1C14", "#E98C7B", "#D4D2D2", "#ADCC54", "#F0D0CE")
+# global package variables
 
+# default colours for mutation spectrum plotting
+COLORS6 = c("#2EBAED", "#000000", "#DE1C14", "#D4D2D2", "#ADCC54", "#F0D0CE")
+COLORS7 = c("#2EBAED", "#000000", "#DE1C14", "#E98C7B", "#D4D2D2", "#ADCC54", "#F0D0CE")
+
+SUBSTITUTIONS = c('C>A','C>G','C>T','T>A','T>C','T>G')
+SUBSTITUTIONS_96 = rep(SUBSTITUTIONS, each=16)
+C_TRIPLETS = c("ACA", "ACC", "ACG", "ACT", "CCA", "CCC", "CCG", "CCT", "GCA", "GCC", "GCG", "GCT", "TCA", "TCC", "TCG", "TCT") 
+T_TRIPLETS = c("ATA", "ATC", "ATG", "ATT", "CTA", "CTC", "CTG", "CTT", "GTA", "GTC", "GTG", "GTT", "TTA", "TTC", "TTG", "TTT") 
+TRIPLETS_96 = c(rep(C_TRIPLETS, 3), rep(T_TRIPLETS, 3))
+STRAND = rep(c("U","T"), 96)
--- R/mut_96_occurences.R
+++ R/mut_96_occurences.R
@@ -2,29 +2,23 @@
 #'  
 #' @param type_context result from get_type_context function
 #' @return vector with 96 trinucleotide mutation occurences
-#' @export
 
 mut_96_occurences = function(type_context)
 {
-  types = c('C>A','C>G','C>T','T>A','T>C','T>G')
-  C_triplets = c("ACA", "ACC", "ACG", "ACT", "CCA", "CCC", "CCG", "CCT", "GCA", "GCC", "GCG", "GCT", "TCA", "TCC", "TCG", "TCT") 
-  T_triplets = c("ATA", "ATC", "ATG", "ATT", "CTA", "CTC", "CTG", "CTT", "GTA", "GTC", "GTG", "GTT", "TTA", "TTC", "TTG", "TTT") 
-  triplets = c(rep(C_triplets,3), rep(T_triplets,3))
   vector = rep(0,96)
-  names(vector) = triplets
-  
+  names(vector) = TRIPLETS_96
   # for all mutations in this sample
   for(i in 1:length(type_context[[1]]))
   {
     # Find mutation type
-    type = which(types == type_context[[1]][i])
+    type = which(SUBSTITUTIONS == type_context[[1]][i])
     # Find triplet
     if(type < 4)
     {
-      context = which(C_triplets == type_context[[2]][i])
+      context = which(C_TRIPLETS == type_context[[2]][i])
     } else
     {
-      context = which(T_triplets == type_context[[2]][i])
+      context = which(T_TRIPLETS == type_context[[2]][i])
     }   
     pos = (type - 1)*16 + context
     vector[pos] = vector[pos] + 1
