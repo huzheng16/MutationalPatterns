8e8d68ad0407b0d0afac580a0bbdecac40b0a6e9
Francis Blokzijl <francisblokzijl87@gmail.com>
2016-08-15 13:53:37 +0200

    strand bias features also global parameter

--- R/MutationalPatterns.R
+++ R/MutationalPatterns.R
@@ -6,6 +6,7 @@ COLORS7 = c("#2EBAED", "#000000", "#DE1C14", "#E98C7B", "#D4D2D2", "#ADCC54", "#
 
 SUBSTITUTIONS = c('C>A','C>G','C>T','T>A','T>C','T>G')
 SUBSTITUTIONS_96 = rep(SUBSTITUTIONS, each=16)
+SUBSTITUTIONS_192 = rep(SUBSTITUTIONS, each=32)
 C_TRIPLETS = c("ACA", "ACC", "ACG", "ACT", "CCA", "CCC", "CCG", "CCT", "GCA", "GCC", "GCG", "GCT", "TCA", "TCC", "TCG", "TCT") 
 T_TRIPLETS = c("ATA", "ATC", "ATG", "ATT", "CTA", "CTC", "CTG", "CTT", "GTA", "GTC", "GTG", "GTT", "TTA", "TTC", "TTG", "TTT") 
 TRIPLETS_96 = c(rep(C_TRIPLETS, 3), rep(T_TRIPLETS, 3))
