27971a23ef77eb455ba88d61182d9b7b448296d0
Roel Janssen <roel@gnu.org>
2017-03-07 18:35:04 +0100

    Add unit tests for read_vcfs_as_granges.

---
+++ tests/testthat/test_read_vcfs_as_granges.R
@@ -0,0 +1,62 @@
+context("Function 'read_vcfs_as_granges'")
+
+ref_genome = "BSgenome.Hsapiens.UCSC.hg19"
+library(BSgenome)
+library(ref_genome, character.only = TRUE)
+
+sample_names <- c ( "colon1", "colon2", "colon3",
+                   "intestine1", "intestine2", "intestine3",
+                   "liver1", "liver2", "liver3" )
+
+vcfs <- list.files (system.file("extdata", package="MutationalPatterns"),
+                    pattern = ".vcf", full.names = TRUE)
+
+test_that("loads multiple samples", {
+    input <- read_vcfs_as_granges(vcfs, sample_names, ref_genome)
+    expect_that(length(input), equals(9))
+})
+
+test_that("nuclear filter works", {
+    input <- read_vcfs_as_granges(vcfs, sample_names, ref_genome)
+    expected <- c(
+        "chr1",  "chr2",  "chr3",  "chr4",  "chr5",  "chr6",  "chr7",
+        "chr8",  "chr9",  "chr10", "chr11", "chr12", "chr13", "chr14",
+        "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21",
+        "chr22", "chrX", "chrY")
+
+    expect_that(seqlevels(input), equals(expected))
+})
+
+test_that("autosomal filter works", {
+    input <- read_vcfs_as_granges(vcfs, sample_names, ref_genome, "auto")
+    expected <- c(
+        "chr1",  "chr2",  "chr3",  "chr4",  "chr5",  "chr6",  "chr7",
+        "chr8",  "chr9",  "chr10", "chr11", "chr12", "chr13", "chr14",
+        "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21",
+        "chr22")
+
+    expect_that(seqlevels(input), equals(expected))
+})
+
+test_that("sex chromosomes filter works", {
+    input <- read_vcfs_as_granges(vcfs, sample_names, ref_genome, "sex")
+    expected <- c("chrX", "chrY")
+
+    expect_that(seqlevels(input), equals(expected))
+})
+
+## test_that("unfiltered works", {
+##     ref_genome = "BSgenome.Hsapiens.1000genomes.hs37d5"
+##     library(ref_genome, character.only = TRUE)
+
+##     input <- read_vcfs_as_granges(vcfs, sample_names, ref_genome, "all")
+##     expected <- c(
+##         "chr1",  "chr2",  "chr3",  "chr4",  "chr5",  "chr6",  "chr7",
+##         "chr8",  "chr9",  "chr10", "chr11", "chr12", "chr13", "chr14",
+##         "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21",
+##         "chr22")
+
+##     print(paste("all = ", seqlevels(input)))
+
+##     expect_that(seqlevels(input), equals(expected))
+## })
