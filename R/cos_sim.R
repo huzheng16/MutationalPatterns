adfd617aa3d8fed2d3e2f0a89b9c733ef54dc6f1
Francis Blokzijl <f.blokzijl@umcutrecht.nl>
2017-10-18 12:54:18 +0200

    add example

--- R/cos_sim.R
+++ R/cos_sim.R
@@ -1,13 +1,17 @@
 #' Cosine similarity function
 #' 
-#' Calculate the cosine similarity between two vectors
+#' Calculate the cosine similarity between two vectors of the same length.
+#' The cosine similarity is a value between 0 (distinct) and 1 (identical) 
+#' and indicates how much two vectors are alike.
 #' 
-#' @param x Vector 1
-#' @param y Vector 2
-#' @return Cosine similarity value
+#' @param x Vector 1 of length n
+#' @param y Vector 2 of length n
+#' @return Cosine similarity value; a value between 0 and 1
 #' 
-#' @example 
-#' 
+#' @examples 
+#' x = c(1.1, 2.1, 0.2, 0.1, 2.9)
+#' y = c(0.9, 1.9, 0.5, 0.4, 3.1)
+#' cos_sim(x, y)
 #' 
 #' @export
 
